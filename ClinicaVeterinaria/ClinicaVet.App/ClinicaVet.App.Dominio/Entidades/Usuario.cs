93756e56a8ff77006f32987a6fe54be81bf4f4b3
EverDuvan <67498901+EverDuvan@users.noreply.github.com>
2021-09-21 14:24:32 -0500

    Add files via upload

---
+++ ClinicaVeterinaria/ClinicaVet.App/ClinicaVet.App.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaVet.App.Dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Celular { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+
+    }
+}
---
+++ ClinicaVeterinaria/ClinicaVet.App/ClinicaVet.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClinicaVet.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
