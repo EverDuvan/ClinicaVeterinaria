7cc7894af53b209b722b9b7869645965e562ab5d
Nicolas <bernalmedina.n@gmail.com>
2021-09-25 14:15:39 -0500

    Nuevo Repositorio Dueño

---
+++ ClinicaVeterinaria/ClinicaVet.App/ClinicaVet.App.Persistencia/AppRepositorio/RepositorioDueño.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using ClinicaVet.App.Dominio;
+
+
+namespace ClinicaVet.App.Persistencia
+{
+    public class RepositorioDueño : IRepositorioDueño
+    {
+        public Dueño AddDueños(Dueño dueño)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteDueños(int idDueño)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Dueño> GetAllDueños()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dueño GetDueños(int idDueño)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dueño UpdateDueños(Dueño dueño)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
