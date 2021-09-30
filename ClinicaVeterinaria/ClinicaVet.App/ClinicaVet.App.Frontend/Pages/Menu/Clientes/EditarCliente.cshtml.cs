5bb00bcabff3fa7302410ecc8d9cac8d36657f90
Nicolas <bernalmedina.n@gmail.com>
2021-09-30 18:17:03 -0500

    Fin CRUD Dueño

--- ClinicaVeterinaria/ClinicaVet.App/ClinicaVet.App.Frontend/Pages/Menu/Clientes/EditarCliente.cshtml.cs
+++ ClinicaVeterinaria/ClinicaVet.App/ClinicaVet.App.Frontend/Pages/Menu/Clientes/EditarCliente.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClinicaVet.App.Dominio;
+using ClinicaVet.App.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,37 @@ namespace ClinicaVet.App.Frontend.Pages
 {
     public class EditarClienteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioDueño repositorioDueño;
+
+        public Dueño dueño { get; set; }
+
+        public EditarClienteModel(IRepositorioDueño repositorioDueño)
         {
+            this.repositorioDueño = repositorioDueño;
+        }
+        
+        public void OnGet(int cedula)
+        {
+            dueño = repositorioDueño.GetDueños(cedula);
+        }
+        public IActionResult OnPost(Dueño dueño)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    repositorioDueño.EditDueños(dueño);
+                    return RedirectToPage("./ListaClientes");
+                }
+                catch
+                {
+                    return RedirectToPage("../Error");
+                }
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
--- ClinicaVeterinaria/ClinicaVet.App/ClinicaVet.App.Frontend/Pages/Menu/Clientes/EliminarCliente.cshtml.cs
+++ ClinicaVeterinaria/ClinicaVet.App/ClinicaVet.App.Frontend/Pages/Menu/Clientes/EliminarCliente.cshtml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using ClinicaVet.App.Dominio;
+using ClinicaVet.App.Persistencia;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,8 +11,24 @@ namespace ClinicaVet.App.Frontend.Pages
 {
     public class EliminarClienteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioDueño repositorioDueño;
+
+        public Dueño dueño { get; set; }
+
+        public EliminarClienteModel(IRepositorioDueño repositorioDueño)
         {
+            this.repositorioDueño = repositorioDueño;
+        }
+
+        public void OnGet(int cedula)
+        {
+            dueño = repositorioDueño.GetDueños(cedula);
+        }
+
+        public IActionResult OnPost(int cedula){
+            
+            repositorioDueño.DeleteDueños(cedula);
+            return RedirectToPage("./ListaClientes");
         }
     }
 }
